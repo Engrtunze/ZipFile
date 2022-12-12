87a69cd420da8078b9beab92e6d4ad2fb8e09094
Tunde John <engrtunze@Tundes-MacBook-Pro.local>
2022-12-12 14:07:52 +0100

    code clean up and modification

--- src/main/java/com/ebie/file_conveter/service/FileConverterImpl.java
+++ src/main/java/com/ebie/file_conveter/service/FileConverterImpl.java
@@ -2,6 +2,7 @@ package com.ebie.file_conveter.service;
 
 import com.ebie.file_conveter.util.FileCompressor;
 import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,10 +10,9 @@ import java.io.IOException;
 
 @Service
 public class FileConverterImpl implements FileConverter {
-
-
     @Override
-    public Resource compressFile(MultipartFile file,  String fileName) throws IOException {
-      return   FileCompressor.compressZip(file, fileName);
+    public ResponseEntity<Resource> uploadAndZipFile(MultipartFile file, String fileName) throws IOException {
+       var filePath= FileCompressor.zipFIle(file, fileName);
+      return   FileCompressor.compressAndDownload(filePath);
     }
 }
