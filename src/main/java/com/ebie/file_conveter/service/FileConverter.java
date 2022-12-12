f9ba46a3684140696eea33ccf23c957b380c220b
Tunde John <engrtunze@Tundes-MacBook-Pro.local>
2022-12-12 02:01:22 +0100

    returned downloadable zip file

--- src/main/java/com/ebie/file_conveter/service/FileConverter.java
+++ src/main/java/com/ebie/file_conveter/service/FileConverter.java
@@ -1,12 +1,13 @@
 package com.ebie.file_conveter.service;
 
 
+import jakarta.annotation.Nullable;
 import org.springframework.core.io.Resource;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 public interface FileConverter {
-    ResponseEntity<Resource> compressFile(MultipartFile file) throws IOException;
+    //ResponseEntity<Resource> compressFile(MultipartFile file) throws IOException;
+    Resource compressFile(MultipartFile file, @Nullable String fileName) throws IOException;
 }
--- src/main/java/com/ebie/file_conveter/service/FileConverterImpl.java
+++ src/main/java/com/ebie/file_conveter/service/FileConverterImpl.java
@@ -2,7 +2,6 @@ package com.ebie.file_conveter.service;
 
 import com.ebie.file_conveter.util.FileCompressor;
 import org.springframework.core.io.Resource;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,10 +10,9 @@ import java.io.IOException;
 @Service
 public class FileConverterImpl implements FileConverter {
 
+
     @Override
-    public ResponseEntity<Resource> compressFile(MultipartFile file) throws IOException {
-        FileCompressor.zip(file);
-
+    public Resource compressFile(MultipartFile file,  String fileName) throws IOException {
+      return   FileCompressor.compressZip(file, fileName);
     }
-
 }
